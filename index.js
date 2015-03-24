0a2bf2320159487e2dcfeffd4999c34a74084511
mmuller <mikolaj.muller23@gmail.com>
2015-03-24 20:04:12 +0100

    added new library

--- index.js
+++ index.js
@@ -1,63 +1,39 @@
 var Q = require('q');
-var fs = require('fs');
+var nconf = require('nconf');
+var readlineSync = require('readline-sync');
 
 module.exports = {
-  helloName: function(name) {
-    return "Hello " + name;
+  load: function() {
+    nconf.use('file', { file: './config.json' });
+    nconf.load();
   },
+  get : function(name) {
+    this.load();
+    return nconf.get(name);
+  },
+  set : function(question, sample, name ) {
+    this.load();
 
-  fileExist : function() {
-    var q =  Q.defer();
-    fs.exists('conf.props', function (exists) {
-      q.resolve(exists ? true : false);
+    if(nconf.get(name)) {
+      return;
+    }
+
+    var value = readlineSync.question(question + " (" + sample + "): ");
+    if(!value) value = sample;
+
+    nconf.set(name, value);
+
+    nconf.save(function (err) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log('Configuration saved successfully.');
     });
-    return q.promise;
-  },
-  writeFile : function(data) {
-    var q =  Q.defer();
-    fs.writeFile('conf.props', JSON.stringify(data), function (err) {
-      if (err) q.reject('Problem save file!!!');
-      else q.resolve(true);
-    });
-    return q.promise;
-  },
-  readFile : function() {
-    var q =  Q.defer();
-    fs.readFile('conf.props', 'utf8', function (err, data) {
-      if (err) q.reject('Problem read file!!!');
-      else q.resolve(JSON.parse(data));
-    });
-    return q.promise;
   }
+
 };
 
-//console.log(module.exports.fileExist());
 
-//module.exports.fileExist().then(function(exists) {
-//  console.log(exists);
-//}) 
-
-
-//module.exports.writeFile("tralala");
-
-//
-
-
-//module.exports.writeFile("tralala").then(function() {
-//
-//});
-
-
-//module.exports.readFile().then(function(data) {
-//  console.log(data.zmienna);
-//});
-
-var c = {};
-
-c.imie = "dupa";
-c.nazwisko = "xxx";
-
-
-module.exports.writeFile(c).then(function() {
-
-});
+//console.log(module.exports.get('dessert'));
+module.exports.set('Podaj imie', "mikolaj", 'name');
