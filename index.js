eb2b377716ed5cc38420e75ad7046032ae511d24
Mikolaj Muller <mikolaj.muller@ilim.poznan.pl>
2015-03-25 12:00:05 +0100

    use load property one time

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-var Q = require('q');
 var nconf = require('nconf');
 var readlineSync = require('readline-sync');
 
+nconf.use('file', { file: './config.json' });
+nconf.load();
+
 module.exports = {
-  load: function() {
-    nconf.use('file', { file: './config.json' });
-    nconf.load();
-  },
   get : function(name) {
-    this.load();
     return nconf.get(name);
   },
   set : function(question, sample, name ) {
-    this.load();
 
-    if(nconf.get(name)) {
+   if(nconf.get(name)) {
+    return;
+  }
+
+  var value = readlineSync.question(question + " (" + sample + "): ");
+  if(!value) value = sample;
+
+  nconf.set(name, value);
+
+  nconf.save(function (err) {
+    if (err) {
+      console.error(err.message);
       return;
     }
-
-    var value = readlineSync.question(question + " (" + sample + "): ");
-    if(!value) value = sample;
-
-    nconf.set(name, value);
-
-    nconf.save(function (err) {
-      if (err) {
-        console.error(err.message);
-        return;
-      }
-      console.log('Configuration saved successfully.');
-    });
-  }
+    console.log('Configuration saved successfully.');
+  });
+}
 
 };
 
 
 //console.log(module.exports.get('dessert'));
-module.exports.set('Podaj imie', "mikolaj", 'name');
+
+//module.exports.set('dessert',"sds","dsds");
+//module.exports.set('dessert',"sdhghs","dsdgggs");
